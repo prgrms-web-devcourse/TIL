9e427583afb030e6f1a1995a7fd6d793553bf103
ilyongHwang <1yongs_@naver.com>
2021-08-09 12:00:09 +0900

    ADD: 2.6 SRP 책임 분리 구현

--- java/실전자바소프트웨어개발/BankTransactionAnalyzerSimple.java
+++ java/실전자바소프트웨어개발/BankTransactionAnalyzerSimple.java
@@ -4,33 +4,117 @@
     - 특정 달엔 몇 건의 입출금 내역이 발생했는가?
     - 지출이 가장 높은 상위 10건은 무엇인가?
     - 돈을 가장 많이 소비하는 항목은 무엇인가?
-    
+
   - SRP 적용하기 위한 책임 분리
     1. 입력 읽기
     2. 주어진 형식의 입력 파싱
     3. 결과 처리
     4. 결과 요약리포트
  */
+
+// 0. 입출금 내역 도메인 클래스
+public class BankTransaction {
+    private final LocalDate date;
+    private final double amount;
+    private final String description;
+
+    public BankTransaction(final LocalDate date, final double amount, final String description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "BankTransaction{" +
+                "date=" + date +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BankTransaction that = (BankTransaction) o;
+        return Double.compare(that.amount, amount) == 0 &&
+                date.equals(that.date) &&
+                description.equals(that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(date ,amount, description);
+    }
+}
+
+// 1. 입력 읽기, 3. 결과 처리, 4. 결과 요약리포트
 public class BankTransactionAnalyzerSimple {
     private static final String RESOURCES = "src/main/resources/";
 
     public static void main(String[] args) throws IOException {
-        final Path path = Paths.get(RESOURCES + args[0]);
+        final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser()
+
+        final String filename = args[0];
+        final Path path = Paths.get(RESOURCES + filename);
         final List<String> lines = Files.readAllLines(path);
 
+        final List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLinesFromCSV(lines);
+
+        System.out.println("The total for all transaction is " + calculateTotalAmount(bankTransactions));
+        System.out.println("Transaction in January " + selectInMonth(bankTransactions, Month.JANUARY));
+    }
+
+    public static double calculateTotalAmount(final List<BankTransaction> bankTransactions) {
         double total = 0d;
-
-        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-
-        for (final String line : lines) {
-            final String[] columns = line.split(",");
-            final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
-            if (date.getMonth() == Month.JANUARY) {
-                final double amount = Double.parseDouble(columns[1]);
-                total += amount;
-            }
+        for (final BankTransaction bankTransaction: bankTransactions) {
+            total += bankTransaction.getAmount();
         }
+        return total;
+    }
 
-        System.out.println("The total for all transaction is " + total);
+    public static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions, final Month month) {
+        final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
+        for (final BankTransaction bankTransaction: bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month)
+                bankTransactionsInMonth.add(bankTransaction);
+        }
+        return bankTransactionsInMonth;
+    }
+}
+
+// 2. 파싱 로직을 추출해 한 클래스로 만듦
+public class BankStatementCSVParser {
+    private static final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+    private BankTransaction parseFromCSV(final String line) {
+        final String[] columns = line.split(",");
+
+        final LocalDate date = LocalDate.parse(columns[0], DATE_PATTERN);
+        final double amount = Double.parseDouble(columns[1]);
+        final String description = columns[2];
+
+        return new BankTransaction(date, amount, description);
+    }
+
+    public List<BankTransaction> parseLinesFromCSV(final List<String> lines) {
+        final List<BankTransaction> bankTransactions = new ArrayList<>();
+        for (final String line : lines) {
+            bankTransactions.add(parseFromCSV(line));
+        }
+        return bankTransactions;
     }
 }
